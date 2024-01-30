0d24a16c12e9c4540de44e36d08ec36ebb2d5a89
Ra-Asis Software <ra.asis.softwaresolutions@outlook.com>
2024-01-30 17:32:13 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Services from './components/Services';
+import AboutUs from './components/AboutUs';
+import Industries from './components/Industries';
+import BuiltByUs from './components/BuiltByUs';
+import HireADeveloper from './components/HireADeveloper';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/about-us">About Us</Link></li>
+            <li><Link to="/industries">Industries</Link></li>
+            <li><Link to="/built-by-us">Built By Us</Link></li>
+            <li><Link to="/hire-a-developer">Hire A Developer</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/services" component={Services} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/industries" component={Industries} />
+          <Route path="/built-by-us" component={BuiltByUs} />
+          <Route path="/hire-a-developer" component={HireADeveloper} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
