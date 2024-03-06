24a2ae00c5be99a7ab6cc7c9e7ed8b9ebdd9b14a
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-06 16:59:49 +0300

    Include sections for the rest of the services

--- src/components/ServicesOverviewSection.js
+++ src/components/ServicesOverviewSection.js
@@ -39,8 +39,58 @@ function ServicesOverviewSection() {
         <div className="service-icon">
           <span className="material-symbols-sharp">devices_other</span>
         </div>
-        <h3>Web Design & Development</h3>
+        <h3>Website Design & Development</h3>
         <p>Custom static or dynamic website tailored to your needs.</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*M & C Management*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">handyman</span>
+        </div>
+        <h3>Maintenance & Content Managementment</h3>
+        <p>Get analysis and maintenance for your existing software</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*Computer Architecture*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">computer</span>
+        </div>
+        <h3>Computer Architecture</h3>
+        <p>Computer architecture solutions and fixes to easen your production processes.</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*Web Application*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">stacks</span>
+        </div>
+        <h3>Web Application Development</h3>
+        <p>Get a full stack web application custom tailored to suit your business needs.</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*Software Proj. Management*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">monitoring</span>
+        </div>
+        <h3>Software Project Management</h3>
+        <p>Let us manage your software project, together we can realise 100% success</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*Graphics*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">developer_mode_tv</span>
+        </div>
+        <h3>Graphics Design</h3>
+        <p>Experience captivating visuals from us and get your custom branding tools</p>
         <button>Learn More</button>
       </div>
 
