abaec8b31d09ff0fc82a9731dd8c60a1b0570d70
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-06 16:35:04 +0300

    Initial snapshot

---
+++ src/components/ServicesOverviewSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function ServicesOverviewSection() {
+  return (
+    <div className="services-overview">
+      
+      {/* Software Dev*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">developer_mode_tv</span>
+        </div>
+        <h3>Software Development</h3>
+        <p>Custom software solutions tailored to your needs.</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*Mobile Apps*/}
+
+    </div>
+  );
+}
+
+export default ServicesOverviewSection;
