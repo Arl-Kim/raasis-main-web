70e1a527a920ebea0a60f1d4758183761fddd7a1
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-06 16:45:12 +0300

    Add more sub-sections for 4 services

--- src/components/ServicesOverviewSection.js
+++ src/components/ServicesOverviewSection.js
@@ -15,6 +15,34 @@ function ServicesOverviewSection() {
       </div>
 
       {/*Mobile Apps*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">developer_mode</span>
+        </div>
+        <h3>Mobile Application Development</h3>
+        <p>Custom mobile apps tailored to your needs.</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*MVP Development*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">rocket_launch</span>
+        </div>
+        <h3>MVP Development</h3>
+        <p>Get an MVP for faster returns</p>
+        <button>Learn More</button>
+      </div>
+
+      {/*Website Design & Development*/}
+      <div className="service">
+        <div className="service-icon">
+          <span className="material-symbols-sharp">devices_other</span>
+        </div>
+        <h3>Web Design & Development</h3>
+        <p>Custom static or dynamic website tailored to your needs.</p>
+        <button>Learn More</button>
+      </div>
 
     </div>
   );
