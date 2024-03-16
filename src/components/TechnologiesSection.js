bf46a693c629d424e366be835788cda7a8a8bef2
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-16 16:20:49 +0300

    Implementations for carousel and navigation functionalities

--- src/components/TechnologiesSection.js
+++ src/components/TechnologiesSection.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function TechnologiesSection() {
+    const categories = [
+        { id: 'mobile', name: 'Mobile', images: ['ios_swift.svg', 'kotlin_logo.svg', 'flutter.svg', 'react_native.svg', 'pwa-logo.svg'] },
+        { id: 'frontend', name: 'Front-End', images: ['frontend1.jpg', 'frontend2.jpg', 'frontend3.jpg'] },
+        { id: 'backend', name: 'Back-End', images: ['backend1.jpg', 'backend2.jpg', 'backend3.jpg'] },
+        { id: 'cms', name: 'CMS & ECommerce', images: ['cms1.jpg', 'cms2.jpg', 'cms3.jpg'] },
+        { id: 'database', name: 'Database', images: ['database1.jpg', 'database2.jpg', 'database3.jpg'] },
+        { id: 'cloud', name: 'Cloud', images: ['cloud1.jpg', 'cloud2.jpg', 'cloud3.jpg'] },
+        { id: 'graphics', name: 'Graphics Design', images: ['graphics1.jpg', 'graphics2.jpg', 'graphics3.jpg'] },
+      ];
+
+      //Manage Selected Category
+      const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+      
   return (
     <div className="technologies-section">
       <h2>We Build & Deploy Across The Tech Stack</h2>
@@ -8,18 +22,26 @@ function TechnologiesSection() {
       
       <nav className="technologies-nav">
         <ul>
-          <li><Link to="/technologies/mobile" className="active">Mobile</Link></li>
-          <li><Link to="/technologies/frontend">Front-End</Link></li>
-          <li><Link to="/technologies/backend">Back-End</Link></li>
-          <li><Link to="/technologies/cms">CMS & ECommerce</Link></li>
-          <li><Link to="/technologies/database">Database</Link></li>
-          <li><Link to="/technologies/cloud">Cloud</Link></li>
-          <li><Link to="/technologies/graphics">Graphics Design</Link></li>
+            {categories.map(category => (
+                <li key={category.id}>
+                    <Link
+                        to={`/technologies/${category.id}`}
+                        className={selectedCategory.id === category.id ? 'active' : ''}
+                        onClick={() => setSelectedCategory(category)}
+                    >
+                        {category.name}
+                    </Link>
+                </li>
+            ))}
         </ul>
       </nav>
       
       <div className="technologies-carousel">
-        {/* Carousel content will go here */}
+        <div className="carousel-inner" style={{ transform: `translateX(-${categories.indexOf(selectedCategory) * 100}%)` }}>
+            {selectedCategory.images.map((image, index) => (
+                <img key={index} src={`/assets/${image}`} alt={selectedCategory.name} />
+            ))}
+        </div>
       </div>
     </div>
   );
