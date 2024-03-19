dd7d4c9c4469ea527b613604049bb9e329d14af1
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-19 22:08:05 +0300

    Change image for client-recognition left

--- src/components/ClientRecognitionSection.js
+++ src/components/ClientRecognitionSection.js
@@ -7,12 +7,19 @@ function ClientRecognitionSection() {
         <div className="client-recognition-left">
           <RotatingHeader />
           <p>From individual brands and startups to leading industry enterprises, Our Client Community keeps gaining new members. What makes them stay at Ra'Asis is our top-notch IT talent that facilitates upscaling of their teams or building custom software solutions. We know you have your unique needs as an individual or group and so we deliver a full spectrum of integrated design, development, and maintenance services for automation and modernization.</p>
-          <img src="/assets/client_image.jpg" alt="Client Image" />
+          <img src="/assets/banner_female_opt.avif" alt="Client Image" />
         </div>
         <div className="client-recognition-right">
-          {/* Logos of brands displayed in a grid format */}
           <div className="brand-logos">
-            {/* Logos go here */}
+            <img src="/assets/transfro_logo.svg" alt="Transfrontières Agencies" />
+            <img src="/assets/oaks_africa.png" alt="Oaks Africa Foundation" />
+            <img src="/assets/npd_logo.png" alt="Nursing Papers Daily" />
+            <img src="/assets/mugeiyot.svg" alt="Mugeiyot" />
+            <img src="/assets/taai_logo.svg" alt="Taai Media" />
+            <img src="/assets/infernos.svg" alt="Mombasa Infernos" />
+            <img src="/assets/kaytech.svg" alt="KayTech CompVilla" />
+            <img src="/assets/zeway_logo.svg" alt="Zeway Builders" />
+            <img src="/assets/mmu_stormerz.svg" alt="MMU Stormerz" />
           </div>
           <Link to="/join" className="cta-button">Join Our Client Community</Link>
         </div>
