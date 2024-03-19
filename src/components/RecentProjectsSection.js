bed7ea655dd7ef24bd247f1735023bc6ac9ce3b0
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-19 11:02:32 +0300

    Move grid items inside one div for accurate styling

--- src/components/RecentProjectsSection.js
+++ src/components/RecentProjectsSection.js
@@ -7,28 +7,30 @@ const RecentProjectsSection = () => {
         <h2>Here's What We've Built & Deployed Recently</h2>
         <p>We empower startups and upcoming brands, with cutting-edge digital software products, that propel them to guaranteed success in their niche.</p>
       </div>
-      <div className="recent-projects-grid">
-        <div className='recent-project-item'>
-            <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
-            <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+      <div className='rp-grid-area'>
+        <div className="recent-projects-grid">
+            <div className='recent-project-item'>
+                <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
+                <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+            </div>
         </div>
-      </div>
-      <div className="recent-projects-grid">
-        <div className='recent-project-item'>
-            <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
-            <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+        <div className="recent-projects-grid">
+            <div className='recent-project-item'>
+                <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
+                <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+            </div>
         </div>
-      </div>
-      <div className="recent-projects-grid">
-        <div className='recent-project-item'>
-            <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
-            <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+        <div className="recent-projects-grid">
+            <div className='recent-project-item'>
+                <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
+                <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+            </div>
         </div>
-      </div>
-      <div className="recent-projects-grid">
-        <div className='recent-project-item'>
-            <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
-            <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+        <div className="recent-projects-grid">
+            <div className='recent-project-item'>
+                <img src='/assets/moocourses.png' alt='Transfrontières MooCourses'/>
+                <img className='gif' src='/assets/moocourses.gif' alt='Transfrontières MooCourses'/>
+            </div>
         </div>
       </div>
     </div>
