dd975bd1f0ac9c6d8125c59fe05b032a556e8d9f
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-19 20:15:21 +0300

    Modify section heading area to hold 2 more heading texts

--- src/components/ClientRecognitionSection.js
+++ src/components/ClientRecognitionSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 function ClientRecognitionSection() {
@@ -6,7 +6,7 @@ function ClientRecognitionSection() {
     <div className="client-recognition-section">
       <div className="client-recognition-content">
         <div className="client-recognition-left">
-          <h2>We Empower Brands Across Kenya, Africa & The World</h2>
+          <RotatingHeader />
           <p>From individual brands and startups to leading industry enterprises, Our Client Community keeps gaining new members. What makes them stay at Ra'Asis is our top-notch IT talent that facilitates upscaling of their teams or building custom software solutions. We know you have your unique needs as an individual or group and so we deliver a full spectrum of integrated design, development, and maintenance services for automation and modernization.</p>
           <img src="/assets/client_image.jpg" alt="Client Image" />
         </div>
@@ -20,6 +20,31 @@ function ClientRecognitionSection() {
       </div>
     </div>
   );
+}
+
+function RotatingHeader(){
+    const phrases = [
+        'We Empower Brands Across Kenya, Africa & The World',
+        'In Equal Measure They Empower Us',
+        'Join Our Growing Client Community Today',
+      ];
+    
+      const [currentPhrase, setCurrentPhrase] = useState(phrases[0]);
+    
+      useEffect(() => {
+        const intervalId = setInterval(() => {
+          const randomIndex = Math.floor(Math.random() * phrases.length);
+          setCurrentPhrase(phrases[randomIndex]);
+        }, 5000); // Will adjust accordingly later
+    
+        return () => clearInterval(intervalId);
+      }, []);
+    
+      return (
+        <h2>
+          {currentPhrase}
+        </h2>
+      );
 }
 
 export default ClientRecognitionSection;
