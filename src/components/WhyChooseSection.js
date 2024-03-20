3f39d33dd70d74832970a932acc14433a1dbca97
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-20 17:43:55 +0300

    Add icons

--- src/components/WhyChooseSection.js
+++ src/components/WhyChooseSection.js
@@ -9,27 +9,27 @@ const WhyChooseSection = () => {
       </div>
       <div className="why-choose-reasons">
         <div className="reason-card">
-          <div className="reason-icon">Icon 1</div>
+          <div className="reason-icon"><i className='bx bx-happy'></i></div>
           <h3>99%</h3>
           <h4>Client Satisfaction</h4>
         </div>
         <div className="reason-card">
-          <div className="reason-icon">Icon 2</div>
+          <div className="reason-icon"><i className='bx bxs-package'></i></div>
           <h3>100++</h3>
           <h4>Completed Projects</h4>
         </div>
         <div className="reason-card">
-          <div className="reason-icon">Icon 3</div>
+          <div className="reason-icon"><i className='bx bxs-user-plus'></i></div>
           <h3>100%</h3>
           <h4>Client Retention Rate</h4>
         </div>
         <div className="reason-card">
-          <div className="reason-icon">Icon 4</div>
+          <div className="reason-icon"><i className='bx bxs-award'></i></div>
           <h3>5++</h3>
           <h4>Years Of Experience</h4>
         </div>
         <div className="reason-card">
-          <div className="reason-icon">Icon 5</div>
+          <div className="reason-icon"><i className='material-symbols-sharp'>handyman</i></div>
           <h3>4 Weeks</h3>
           <h4>Free Maintenance After Deployment</h4>
         </div>
