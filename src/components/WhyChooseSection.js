6f8c997b813a4db5490e3a3f92709ffc379e8825
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-20 18:57:57 +0300

    Add id values to individual cards

--- src/components/WhyChooseSection.js
+++ src/components/WhyChooseSection.js
@@ -8,27 +8,27 @@ const WhyChooseSection = () => {
         <p>Partnering with us comes with guaranteed digital transformation for your brand.</p>
       </div>
       <div className="why-choose-reasons">
-        <div className="reason-card">
+        <div className="reason-card" id='light_orange'>
           <div className="reason-icon"><i className='bx bx-happy'></i></div>
           <h3>99%</h3>
           <h4>Client Satisfaction</h4>
         </div>
-        <div className="reason-card">
+        <div className="reason-card" id='dark_orange'>
           <div className="reason-icon"><i className='bx bxs-package'></i></div>
           <h3>100++</h3>
           <h4>Completed Projects</h4>
         </div>
-        <div className="reason-card">
+        <div className="reason-card" id='light_blue'>
           <div className="reason-icon"><i className='bx bxs-user-plus'></i></div>
           <h3>100%</h3>
           <h4>Client Retention Rate</h4>
         </div>
-        <div className="reason-card">
+        <div className="reason-card" id='light_orange'>
           <div className="reason-icon"><i className='bx bxs-award'></i></div>
           <h3>5++</h3>
           <h4>Years Of Experience</h4>
         </div>
-        <div className="reason-card">
+        <div className="reason-card" id='dark_orange'>
           <div className="reason-icon"><i className='material-symbols-sharp'>handyman</i></div>
           <h3>4 Weeks</h3>
           <h4>Free Maintenance After Deployment</h4>
